ae1056337b4953a68b2feccc9cebf408079ff196
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2024-12-30 20:05:27 -0300

    Criando arquivo

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String [] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+        String path = "/Users/vitoramaral/Java/Arquivos/out.txt";
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch(IOException e ){
+            e.printStackTrace();
+        }
+    }
+}
